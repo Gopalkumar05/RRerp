83937de0dde8e49a5e0b67765480a4546c198f0a
Gopalkumar05 <112787134+Gopalkumar05@users.noreply.github.com>
2025-05-24 21:57:57 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     setLoginSuccess(false);
   
     try {
-      const response = await fetch("http://localhost:5000/api/v1/user/login", {
+      const response = await fetch("https://rrerp.onrender.com/api/v1/user/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
